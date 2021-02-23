d2724c8838b237555e06b96c0a5397b48243adbe
Padma <padma@softvessel.com>
2021-02-23 12:38:49 +0530

    create HomePage, SignIn page, Contact page, Event page, Dashboard Page, course page template.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { SignInPageComponent } from './components/sign-in-page/sign-in-page.component';
+import { ContactPageComponent } from './components/contact-page/contact-page.component';
+import { DashboardPageComponent } from './components/dashboard-page/dashboard-page.component';
+import { EventPageComponent } from './components/event-page/event-page.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { CoursePageComponent } from './layout/course-page/course-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    SignInPageComponent,
+    ContactPageComponent,
+    DashboardPageComponent,
+    EventPageComponent,
+    HeaderComponent,
+    FooterComponent,
+    CoursePageComponent
   ],
   imports: [
     BrowserModule

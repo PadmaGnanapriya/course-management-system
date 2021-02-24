f3a1046fc353a2132798d6123663ce0d4043b87f
Padma <padma@softvessel.com>
2021-02-24 13:15:33 +0530

    setup routing for the application home, contact, events, dashboard, sign-in

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,25 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { HomePageComponent } from './components/home-page/home-page.component';
-import { SignInPageComponent } from './components/sign-in-page/sign-in-page.component';
-import { ContactPageComponent } from './components/contact-page/contact-page.component';
-import { DashboardPageComponent } from './components/dashboard-page/dashboard-page.component';
-import { EventPageComponent } from './components/event-page/event-page.component';
-import { HeaderComponent } from './layout/header/header.component';
-import { FooterComponent } from './layout/footer/footer.component';
-import { CoursePageComponent } from './layout/course-page/course-page.component';
+import {AppComponent} from './app.component';
+import {HomePageComponent} from './components/home-page/home-page.component';
+import {SignInPageComponent} from './components/sign-in-page/sign-in-page.component';
+import {ContactPageComponent} from './components/contact-page/contact-page.component';
+import {DashboardPageComponent} from './components/dashboard-page/dashboard-page.component';
+import {EventPageComponent} from './components/event-page/event-page.component';
+import {HeaderComponent} from './layout/header/header.component';
+import {FooterComponent} from './layout/footer/footer.component';
+import {CoursePageComponent} from './layout/course-page/course-page.component';
+import {RouterModule, Routes} from '@angular/router';
+
+const routes: Routes = [
+  {path: '', component: HomePageComponent},
+  {path: 'sign-in', component: SignInPageComponent},
+  {path: 'contact', component: ContactPageComponent},
+  {path: 'dashboard', component: DashboardPageComponent},
+  {path: 'events', component: EventPageComponent},
+  {path: 'course', component: CoursePageComponent}
+];
 
 @NgModule({
   declarations: [
@@ -24,9 +34,11 @@ import { CoursePageComponent } from './layout/course-page/course-page.component'
     CoursePageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}

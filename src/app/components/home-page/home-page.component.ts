e9d3906b983366bbe11e4e69b38716c8c57c2859
Padma <padma@softvessel.com>
2021-02-26 06:19:08 +0530

    add ngfor card into homepage.

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -7,12 +7,19 @@ import {Component, OnInit} from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
   brightness: number;
+  notificationList: any;
 
 
   constructor() {
   }
 
   ngOnInit(): void {
+    this.notificationList = [
+      {title: 'Notification title 1', notificationBody: 'the notification 1'},
+      {title: 'Notification title 2', notificationBody: 'the notification 2'},
+      {title: 'Notification title 3', notificationBody: 'the notification 3'},
+      {title: 'Notification title 4', notificationBody: 'the notification 4'},
+    ];
   }
 
 }

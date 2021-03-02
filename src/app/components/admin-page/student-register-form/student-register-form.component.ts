d47923a05cd0cac45d0758c776e0d20d8c515f85
PadmaGnanapriya <padmaisuru@gmail.com>
2021-03-02 07:22:10 +0530

    Finish student form validation.

--- src/app/components/admin-page/student-register-form/student-register-form.component.ts
+++ src/app/components/admin-page/student-register-form/student-register-form.component.ts
@@ -1,60 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 import StudentDTO from '../../../dto/StudentDTO';
 import {StudentService} from '../../../services/student.service';
+import Swal from 'sweetalert2/dist/sweetalert2.js';
 
 @Component({
   selector: 'app-student-register-form',
   templateUrl: './student-register-form.component.html',
   styleUrls: ['./student-register-form.component.css']
 })
+
 export class StudentRegisterFormComponent implements OnInit {
-  studentId: string;
-  studentName: string;
-  studentContact: string;
-  studentNic: string;
-  studentEmail: string;
-  studentBirthday: string;
-  studentPassword: string;
-
-  isShowStudentRegistration: boolean;
-
 
   constructor(private studentService: StudentService) { }
+  isShowStudentRegistration: boolean;
 
   ngOnInit(): void {
     this.isShowStudentRegistration = false;
   }
 
-  saveStudent = () => {
-    const student = new StudentDTO(
-      this.studentId,
-      this.studentName,
-      this.studentContact,
-      this.studentNic,
-      this.studentEmail,
-      this.studentBirthday,
-      this.studentPassword,
+  handleClearStudent = () => {
+    Array.from(document.querySelectorAll('input')).forEach(
+      input => (input.value = '')
     );
+  }
+  handleOnIsStudentRegistrationShow = () => {
+    this.isShowStudentRegistration = !this.isShowStudentRegistration;
+  }
 
-    this.studentService.addStudent(student).subscribe(resp => {
-      alert('Add successful');
+  onSubmit = (value: StudentDTO) => {
+
+    this.studentService.addStudent(value).subscribe(resp => {
+      Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: 'New record has been saved',
+        showConfirmButton: false,
+        timer: 1500
+      });
       this.handleClearStudent();
     }, error => {
       console.log(error);
     });
-  }
-
-  handleClearStudent = () => {
-    this.studentId = '';
-    this.studentName = '';
-    this.studentContact = '';
-    this.studentNic = '';
-    this.studentEmail = '';
-    this.studentBirthday = '';
-    this.studentPassword = '';
-  }
-
-  handleOnIsStudentRegistrationShow = () => {
-    this.isShowStudentRegistration = !this.isShowStudentRegistration;
   }
 }

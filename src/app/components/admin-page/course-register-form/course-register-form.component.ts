08a8b70ed3af376f2ead2408bbe5fc4710e35dab
PadmaGnanapriya <padmaisuru@gmail.com>
2021-03-02 07:30:58 +0530

    Finish course form validation.

--- src/app/components/admin-page/course-register-form/course-register-form.component.ts
+++ src/app/components/admin-page/course-register-form/course-register-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {CourseService} from '../../../services/Course.service';
+import CourseDTO from '../../../dto/CourseDTO';
+import Swal from 'sweetalert2/dist/sweetalert2.js';
 
 @Component({
   selector: 'app-course-register-form',
@@ -6,10 +9,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./course-register-form.component.css']
 })
 export class CourseRegisterFormComponent implements OnInit {
-
-  constructor() { }
+  constructor(private courseService: CourseService) { }
+  isShowCourseRegistration: boolean;
 
   ngOnInit(): void {
+    this.isShowCourseRegistration = false;
   }
 
+  handleClearCourse = () => {
+    Array.from(document.querySelectorAll('input')).forEach(
+      input => (input.value = '')
+    );
+  }
+  handleOnIsCourseRegistrationShow = () => {
+    this.isShowCourseRegistration = !this.isShowCourseRegistration;
+  }
+
+  onSubmit = (value: CourseDTO) => {
+
+    this.courseService.addCourse(value).subscribe(resp => {
+      Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: 'New record has been saved',
+        showConfirmButton: false,
+        timer: 1500
+      });
+      this.handleClearCourse();
+    }, error => {
+      console.log(error);
+    });
+  }
 }
